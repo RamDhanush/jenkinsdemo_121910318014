eee19d9791dcb0e53329aced61fb9e2180d81da5
RamDhanush <66680425+RamDhanush@users.noreply.github.com>
2021-09-27 20:55:35 +0530

    Create palindrometest.java

---
+++ palindrometest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PalindromeTest{
+  @Test
+  public void whenEmptyString_thenAccept(){
+    Palindrome palindromeTester = new Palindrome();
+    assertTrue(palindromeTester.isPalindrome("madam"));
+  }
+}
